d51f8301cac7491d0e4d2f9557c4eeec6aa08a5a
pabletefatimarocio <pabletefatima9@gmail.com>
2023-08-07 11:57:16 -0300

    navbar, mh onClick

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,22 +6,25 @@ import { MenuItems } from "./MenuItems";
 
 
 class Navbar extends Component {
+  /*Inicializamos MH*/ 
     state = {clicked: false};
 
     handleClick = ()=> {
       this.setState({clicked: !this.state.clicked})
     }
+    
     render() {
       return (
         <nav className="NavbarItems">
           <h1 className="navbar-logo" >TripFun</h1>
 
-          <div className="menu-icons">
+          <div className="menu-icons" onClick={this.handleClick}>
            <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
           
           </div>
 
-          <ul className="nav-menu"  onClick={this.handleClick}>
+          <ul className={this.state.clicked ? 
+            "nav-menu active" : "nav-menu"} >
             {
               MenuItems.map((item,index) =>{
                 return(
