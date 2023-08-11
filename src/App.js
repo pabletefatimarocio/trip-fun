1d4641c9d5e9bce50a4b9c76b7302ce6067c38ee
pabletefatimarocio <pabletefatima9@gmail.com>
2023-08-11 19:27:36 -0300

    enrutamiento

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 
 import Navbar from "./components/Navbar"
+import About from "./routes/About"
+import Contact from "./routes/Contact"
+import Home from "./routes/Home"
+import Service from "./routes/Service"
 import "./styles.css"
+import { Route, Routes } from "react-router-dom"
 
 export default function App(){
   return(
+     <div className="App">
+     <Routes>
+     <Route  path="/" element={<Home />}/>
+     <Route  path="/about" element={<About/>}/>
+     <Route  path="/service" element={<Service />}/>
+     <Route  path="/contact" element={<Contact />}/>
+     </Routes>
 
-   <div className="App">
-    
+
     <Navbar />
-     
+    <Home/>
    </div>
 
   )
